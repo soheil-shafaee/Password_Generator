8a26f49507dbcc9019efac936ad5b4c83400b70e
Soheil Shafaee <soheil.shafaee.6200@gmail.com>
2023-11-11 16:37:13 +0330

    Added Widgets and Image and Icon

--- password_generator.py
+++ password_generator.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QPushButton, QLineEdit, QSlider, QCheckBox
+from PyQt5.QtGui import QPixmap, QIcon
 from PyQt5 import uic
 import sys
 
@@ -9,7 +10,22 @@ class UI(QMainWindow):
 
         # Load UI File
         uic.loadUi("password_generator.ui", self)
-        
+        self.setFixedSize(698, 528)
+
+        # Define Our Widgets
+        self.picLabel = self.findChild(QLabel, "picLabel")
+        self.passwordLine = self.findChild(QLineEdit, "passwordLine")
+        self.passwordGenerate = self.findChild(QPushButton, "passwordGenerate")
+        self.passwordLenghts = self.findChild(QSlider, "passwordLenghtsVertical")
+        self.number = self.findChild(QCheckBox, "numberCheckbox")
+        self.upper = self.findChild(QCheckBox, "upperCheckBox")
+        self.symbol = self.findChild(QCheckBox, "symbolCheckBox")
+
+        # Add Image To Label and Icon
+        self.setWindowIcon(QIcon("png/002-password.png"))
+        self.pixmapPic = QPixmap("png/001-password-code.png")
+        self.picLabel.setPixmap(self.pixmapPic)
+
         # Show The App
         self.show()
 
