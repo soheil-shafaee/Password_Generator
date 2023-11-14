eeb3984419ff1e561df86e9c469a0f199689f4e6
Soheil Shafaee <soheil.shafaee.6200@gmail.com>
2023-11-14 13:08:08 +0330

    Added Function to generate Password

--- password_generator.py
+++ password_generator.py
@@ -5,7 +5,6 @@ import sys
 import random
 
 
-
 class UI(QMainWindow):
     def __init__(self):
         super(UI, self).__init__()
@@ -31,24 +30,34 @@ class UI(QMainWindow):
 
         # Click The Button
         self.passwordGenerate.clicked.connect(self.generate_password)
-        self.number.toggled.connect(self.generate_password)
-        self.symbol.toggled.connect(self.generate_password)
-        self.upper.toggled.connect(self.generate_password)
+
         # The Number Of Character
-        self.passwordLengths.valueChanged.connect(self.generate_password)
+        self.passwordLengths.valueChanged.connect(self.move)
         self.passwordLengths.setMinimum(5)
         self.passwordLengths.setMaximum(20)
         self.passwordLengths.setValue(5)
         self.passwordLengths.setTickPosition(QSlider.TicksBelow)
         self.passwordLengths.setTickInterval(1)
 
+        # Set Default Check Of Number, Symbol, Uppercase
+        self.number.setChecked(True)
+        self.upper.setChecked(True)
+        self.symbol.setChecked(True)
+
         # Show The App
         self.show()
 
-    # Define The Generate Password Function
-    def generate_password(self, value):
+    # Define The Change Value Slider Function
+    def move(self, value):
         # Change The Number Of Password Lengths
         self.passwordLengthsLabel.setText(str(value))
+
+    # Define The Generate Password Function
+    def generate_password(self, value):
+        # Password letter List
+        password_list = []
+        self.passwordLengths.valueChanged.connect(self.move)
+        print(str(self.passwordLengths.valueChanged.connect(self.move)))
         # The Letter and Symbols and Number For Making Password
         letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
                    't', 'u', 'v', 'w', 'x', 'y', 'z']
@@ -64,7 +73,11 @@ class UI(QMainWindow):
             symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
             letters += symbols
 
-        
+        # for i in range(value):
+        #     password_letter = random.choice(letters)
+        #     password_list.append(password_letter)
+        # password = "".join(password_list)
+        # self.passwordLine.setText(password)
 
 
 # Initialize The App
